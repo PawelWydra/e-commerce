5f8f74eb658ccfb350669d87b938f957269f4e3a
Pawel <wydrapawel@icloud.com>
2022-11-03 20:12:58 +0100

    add routes, connect index with Css/Bootstrap

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from flask_bootstrap import Bootstrap
+import os
+
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "123"
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+Bootstrap(app)
+
 
 @app.route('/')
-def get_all_posts():
-    return render_template("index.html")
+def home():
+    return render_template("base.html")
+
+
+@app.route('/shop_single')
+def shop_single():
+    return render_template("shop-single.html")
+
+
+@app.route('/shop')
+def shop():
+    return render_template("shop.html")
+
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
